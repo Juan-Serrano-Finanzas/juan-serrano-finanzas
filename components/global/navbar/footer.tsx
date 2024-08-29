d0bb2475590deb13af3e21735125029cfc82fb80
Luis Anaya <brothersailorwebdev@gmail.com>
2024-08-29 11:56:19 +0200

    fix: mobile

--- components/global/navbar/footer.tsx
+++ components/global/navbar/footer.tsx
@@ -1,9 +1,46 @@
 import { currentYear } from '@/lib/utils'
+import {
+  Envelope,
+  InstagramLogo,
+  LinkedinLogo
+} from '@phosphor-icons/react/dist/ssr'
 
 export const Footer = () => {
   return (
-    <footer className='bottom-0 w-full py-5 text-center text-xs text-stone-500 md:py-10'>
-      <span>{`© ${currentYear}, Juan B. Serrano García`}</span>
+    <footer className='bottom-0 w-full bg-stone-950 px-8 py-8 text-white md:px-16 md:py-16'>
+      <div className='flex flex-col items-center justify-between gap-y-2 text-center md:flex-row md:items-start md:text-left'>
+        <div className='flex flex-col gap-y-2'>
+          <p>{`© Juan B. Serrano García ${currentYear}`}</p>
+          <div className='flex items-baseline justify-center gap-x-1 md:justify-start'>
+            <p>{`Site by `}</p>
+            <a
+              href='laam.dev'
+              rel='noopener noreferrer'
+              target='_blank'
+              className='hover:text-white/90'
+            >{` laam.dev`}</a>
+          </div>
+        </div>
+        <div className='flex items-center gap-x-4'>
+          <a
+            href='mailto:juanserranofinanzas@gmail.com'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <Envelope weight='fill' className='size-8 hover:text-white/90' />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/juan-bautista-serrano-garcia-989b11167/?challengeSource=AgHW41wW7NjkNwAAAZB-TGTf6Idv8jam5W4OSrC0h7a1OwsADhLdSnLbnk_vtmw&submissionId=5b01b1a0-280b-df17-362a-6cd44af87c2f&recognizeDevice=AgEQTij5XW_2bQAAAZB-TGTqrxbYjeEYqVyaf63uwkuGGwIMVb0W&challengeId=AQFW3_N9nRP84QAAAZB-TF1QOPG3VrzMNNiweTCUBq5OQ4BRXqR9FOZTwfSj7qSAmFp9rfvXDRDRSuYfXILIF3kBFGh77PTV7w&challegeType=AgHSVRGJ7_wTfgAAAZB-TGTjT_hbI2GOvzPJkzzEmenkc2a-jerqJ9c&memberId=AgEmfkL5z_3tRAAAAZB-TGTmVv9075jP0B65-d7jES_8UIQ&originalSubdomain=es'
+            rel='noopener noreferrer'
+            target='_blank'
+          >
+            <LinkedinLogo
+              weight='fill'
+              className='size-8 hover:text-white/90'
+            />
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
--- components/pages/home/book-card.tsx
+++ components/pages/home/book-card.tsx
@@ -11,8 +11,11 @@ import { urlForImage } from '@/sanity/lib/utils'
 
 export const BookCard = ({ book }) => {
   return (
-    <div key={book.id} className='grid gap-16 md:grid-cols-4'>
-      <div className='relative col-span-1 aspect-[3/4] w-full bg-stone-50'>
+    <div
+      key={book.id}
+      className='flex flex-col gap-16 gap-y-4 md:grid md:grid-cols-4'
+    >
+      <div className='relative aspect-[3/4] w-full bg-stone-50 md:col-span-1'>
         <Image
           alt={`Portada del libro ${book?.title}`}
           src={
@@ -29,12 +32,12 @@ export const BookCard = ({ book }) => {
           className='bg-stone-100 object-cover object-center p-8'
         />
       </div>
-      <div className='col-span-1 flex flex-col justify-between'>
+      <div className='flex flex-col justify-between md:col-span-1'>
         <div>
           <h2 className='mt-2 font-serif text-xl font-medium leading-tight'>
             {book.title}
           </h2>
-          <div className='mt-4 flex flex-col divide-y'>
+          <div className='mb-4 mt-4 flex flex-col divide-y md:mb-0'>
             <h3 className='py-1 text-sm text-stone-500'>{`${book.pages} páginas`}</h3>
             <h3 className='py-1 text-sm text-stone-500'>{`Publicado ${format(parseISO(book.publishedAt), 'd/MM/yyyy')}`}</h3>
             <h3 className='py-1 text-sm text-stone-500'>{`Precio ${book.price.toFixed(2)} €`}</h3>
@@ -45,13 +48,16 @@ export const BookCard = ({ book }) => {
         <Link
           href={`/libros/${book.slug}`}
           className={cn(
-            buttonVariants({ variant: 'default', className: 'w-fit' })
+            buttonVariants({
+              variant: 'default',
+              className: 'w-fit md:mt-0'
+            })
           )}
         >
           {`Ver Libro`}
         </Link>
       </div>
-      <div className='col-span-2 flex flex-col justify-between gap-y-4 md:gap-y-0'>
+      <div className='flex flex-col justify-between gap-y-4 md:col-span-2 md:gap-y-0'>
         <div className='prose md:prose-lg mt-4'>
           <CustomPortableText value={book.summary as PortableTextBlock[]} />
         </div>
